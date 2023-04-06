95649bad99eb5970c6bf7e93f04dbc2ddc05f30c
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-04-05 22:19:07 -0400

    Revert "Update"
    This reverts commit 074d34787dedf98f0ee9a01f6d42456e0052bada.

--- 101190443_comp3133_assig2/server.js
+++ 101190443_comp3133_assig2/server.js
@@ -2,9 +2,17 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname + "/dist/101190443_comp3133-assig2"));
+app.use(
+  express.static(
+    __dirname + "/dist/101190443_comp3133-assig2/101190443_comp3133_assig2"
+  )
+);
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/101190443_comp3133_assig2"));
+  res.sendFile(
+    path.join(
+      __dirname + "/dist/101190443_comp3133_assig2/101190443_comp3133_assig2"
+    )
+  );
 });
 
 app.listen(process.env.PORT || 4200);

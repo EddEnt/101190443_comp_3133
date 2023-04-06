9eabfd5759df6ce74f50e38797fcfd162ee3d701
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-04-05 22:36:13 -0400

    Revert "Update server.js"
    This reverts commit 54cdbadbb089d4afed2373786da65d72f6cfc166.

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const app = express();
 
 app.use(express.static(__dirname + "/dist/101190443_comp3133-assig2"));
 app.get("/*", function (req, res) {
-  res.sendFile(
-    path.join(__dirname + "/dist/101190443_comp3133_assig2/src/index.html")
-  );
+  res.sendFile(path.join(__dirname + "/dist/101190443_comp3133_assig2"));
 });
 
 app.listen(process.env.PORT || 4200);

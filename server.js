6573177a03fc3ebaf271a685c57694eddd297d65
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-04-05 23:00:45 -0400

    Update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname + "/dist/101190443_comp3133-assig2"));
+app.use(express.static(__dirname + "/dist/101190443_comp3133_assig2"));
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/101190443_comp3133_assig2"));
 });

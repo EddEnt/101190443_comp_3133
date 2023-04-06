84e85ceca21db0b68f17711b373664a5aa05ec01
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-04-05 22:44:35 -0400

    Update

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 
 app.use(express.static(__dirname + "/dist/101190443_comp3133-assig2"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/101190443_comp3133_assig2"));
+  res.sendFile(
+    path.join(__dirname + "/dist/101190443_comp3133_assig2/index.html")
+  );
 });
 
 app.listen(process.env.PORT || 4200);
